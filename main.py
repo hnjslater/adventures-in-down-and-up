d5386d0f1241a989251a108485455e260cef03fd
HNJSLater <henry.slater@gmail.com>
2014-04-27 10:02:57 +0100

    Can move left and right now

--- main.py
+++ main.py
@@ -24,14 +24,55 @@ class SomeSprites(pygame.sprite.Group):
         pygame.sprite.Group.draw(self, win)
         for thing in self:
             thing.draw(win)
+    def tick(self):
+        for thing in self:
+            thing.tick()
 
 class Thing(pygame.sprite.Sprite):
     def __init__(self, img_path, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image(img_path)
         self.rect.center = (x, y)
+        self.dx = 0
+        self.ddx = 0
     def draw(self,win):
         pass
+    def tick(self):
+        self.rect.x += self.dx
+        self.dx += self.ddx
+
+        if self.rect.x < 0:
+            self.rect.x = 0
+            if self.dx < 0:
+                self.dx == 0
+
+        if self.rect.right > SCREEN_SIZE:
+            self.rect.right = SCREEN_SIZE
+            if self.dx > 0:
+                self.dx == 0
+
+        if self.dx < -5:
+            self.dx = -5
+        elif self.dx > 5:
+            self.dx = 5
+
+    
+
+class Stage():
+    def __init__(self):
+        self.sprites = SomeSprites()
+        self.player = Thing("dude.png", 400, 400);
+        self.sprites.add(self.player);
+    def keypress(self, key):
+        if key == K_RIGHT:
+            self.player.ddx = 1;
+        if key == K_LEFT:
+            self.player.ddx = -1;
+            
+    def tick(self, win):
+        self.sprites.tick();
+        self.sprites.draw(win);
+        
 
 def main():
     pygame.init()
@@ -42,8 +83,7 @@ def main():
     pygame.display.set_caption('game')
     font = pygame.font.Font(None, 20)
 
-    sprites = SomeSprites()
-    sprites.add(Thing("dude.png", 400, 400));
+    stage = Stage()
 
     frame_count = 0
 
@@ -52,13 +92,12 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-
-        sprites.draw(win);
-
+            elif event.type == KEYDOWN:
+                stage.keypress(event.key)
+        
+        stage.tick(win)
+        
         pygame.display.update()
         fpsClock.tick(60)
-        print(fpsClock.get_fps())
-
-
 
 main()
