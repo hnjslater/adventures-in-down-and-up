d8d3cba5fd5108c7918cb1c3b8c24823ec7cd586
HNJSLater <henry.slater@gmail.com>
2014-04-27 11:41:00 +0100

    Platforms, double jump. Sensible acceloration

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ class Thing(pygame.sprite.Sprite):
         self.dx = 0
         self.ddx = 0
         self.dy = 0
+        self.hops = 0
     def draw(self,win):
         pass
     def tick(self):
@@ -58,26 +59,45 @@ class Thing(pygame.sprite.Sprite):
             self.dx = 10
 
         self.rect.y += self.dy
-        self.dy += 3 
+        self.dy += 2 
         if self.dy > 20:
             self.dy = 20
         
         if self.rect.bottom >= SCREEN_SIZE:
             self.rect.bottom = SCREEN_SIZE;
             self.dy = 0;
-            
 
     def jump(self):
-        self.dy = -50
-        
+        if not self.airbourne and not self.hops > 1:
+            self.dy = -25
+            self.hops += 1
 
-    
+class Platform(Thing):
+    def __init__(self, x, y):
+        Thing.__init__(self, "platform.png", x, y)
+    def tick(self):
+        pass
+    def draw(self, win):
+        pass
 
 class Stage():
     def __init__(self):
         self.sprites = SomeSprites()
         self.player = Thing("dude.png", 400, 400)
         self.sprites.add(self.player)
+
+        x = 0;
+        while x < SCREEN_SIZE:
+            platform = Platform(x, 0)
+            platform.rect.bottom = SCREEN_SIZE
+            x += platform.rect.width
+            self.sprites.add (platform)
+
+        self.sprites.add(Platform(400, 700))
+        self.sprites.add(Platform(400, 300))
+
+        self.sprites.add(Platform(200, 600))
+        self.sprites.add(Platform(200, 200))
     def keypress(self, key):
         if key == K_RIGHT:
             self.player.ddx = 1
@@ -86,9 +106,28 @@ class Stage():
         elif key == K_SPACE:
             self.player.jump()
         
+    def keyup(self, key):
+        if key == K_RIGHT and self.player.ddx == 1:
+            self.player.ddx = 0
+            self.player.dx = 0
+        elif key == K_LEFT and self.player.ddx == -1:
+            self.player.ddx = 0
+            self.player.dx = 0
             
     def tick(self, win):
         self.sprites.tick();
+        
+        feet = pygame.sprite.Sprite()
+        feet.rect = pygame.Rect(self.player.rect.x, self.player.rect.bottom - 2, self.player.rect.width, 2)
+
+        if self.player.dy > 0:
+            for platform in self.sprites:
+                if pygame.sprite.collide_rect(feet, platform) and not platform == self.player:
+                    self.player.rect.bottom = platform.rect.y-1
+                    self.player.dy = 0
+                    self.player.airbourne = False
+                    self.player.hops = 0
+
     def draw(self, win):
         win.fill((0,0,0), (0,0,SCREEN_SIZE,SCREEN_SIZE)) 
         self.sprites.draw(win);
@@ -114,6 +153,8 @@ def main():
                 sys.exit()
             elif event.type == KEYDOWN:
                 stage.keypress(event.key)
+            elif event.type == KEYUP:
+                stage.keyup(event.key)
         
         stage.tick(win)
         stage.draw(win)
